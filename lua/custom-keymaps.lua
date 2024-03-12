4bc5e5301a5000d1919a88b07acda52f5c04ed1e
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2024-03-12 13:27:33 -0400

    add go to file function

--- lua/custom-keymaps.lua
+++ lua/custom-keymaps.lua
@@ -9,3 +9,18 @@ end, { desc = 'Copy file path and line number to clipboard' })
 vim.keymap.set('n', 'f', '<Plug>Sneak_s', { desc = 'search forward' })
 vim.keymap.set('n', 'F', '<Plug>Sneak_S', { desc = 'search backward' })
 vim.opt.clipboard = 'unnamedplus'
+
+-- Function to convert the edit command format
+function ConvertEditCommand()
+  local command = vim.fn.input 'Enter edit command: '
+  local file_path, line_number = string.match(command, '^(.-):(%d+)$')
+  if file_path and line_number then
+    vim.cmd('e ' .. file_path)
+    vim.fn.cursor(tonumber(line_number), 1)
+  else
+    print 'Invalid input format. Use "/path/to/file.txt:line_number"'
+  end
+end
+
+-- Key mapping for ConvertEditCommand function
+vim.api.nvim_set_keymap('n', '<Leader>gf', ':lua ConvertEditCommand()<CR>', { noremap = true })

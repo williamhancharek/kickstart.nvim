43d89aedb7d33cc971c659f90dc6f217ca3c8ff7
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2024-04-01 20:07:01 -0400

    add conform_format options

--- init.lua
+++ init.lua
@@ -769,6 +769,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        eruby = { 'erb_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --

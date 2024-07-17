adf14bc83efded22758861c012d6f669a31f754c
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2024-07-17 06:17:16 -0400

    specify htmlbeautifier

--- init.lua
+++ init.lua
@@ -769,7 +769,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        eruby = { 'erb_format' },
+        eruby = { 'htmlbeautifier' },
+        yml = { 'yamlfix' },
+        yaml = { 'yamlfix' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --

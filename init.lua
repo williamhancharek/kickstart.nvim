2f7e8defcae0d02b28652991cbb75ae05da77135
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2024-09-07 20:00:51 -0400

    turn off formatting for eruby

--- init.lua
+++ init.lua
@@ -774,8 +774,6 @@ require('lazy').setup({
       formatters_by_ft = {
         ruby = { 'solargraph' },
         lua = { 'stylua' },
-        eruby = { 'yamlfix', 'htmlbeautifier' },
-        yaml = { 'yamlfix' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --

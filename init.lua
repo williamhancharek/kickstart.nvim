c611f5fb139e92a805e4b9ab65fcf03522b16aa3
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2024-09-13 15:06:18 -0400

    remove unnecessary formatters

--- init.lua
+++ init.lua
@@ -772,7 +772,6 @@ require('lazy').setup({
         end
       end,
       formatters_by_ft = {
-        ruby = { 'solargraph' },
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },

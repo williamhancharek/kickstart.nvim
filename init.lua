a799577a7bd3599d5c47c7e94ef3777deca9d93f
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2025-04-30 10:32:30 -0400

    set log level to warn

--- init.lua
+++ init.lua
@@ -523,6 +523,7 @@ require('lazy').setup({
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
       --    function will be executed to configure the current buffer
+      vim.lsp.set_log_level 'warn'
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)

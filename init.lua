c2653a153debac9f014d10c8b8365d1fd7dc35e5
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2025-05-16 16:13:25 -0400

    return to tokyo night

--- init.lua
+++ init.lua
@@ -881,19 +881,28 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
-  {
-    -- Change to Gruvbox
-    'ellisonleao/gruvbox.nvim',
-    priority = 1000, -- Load this before other plugins
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      require('gruvbox').setup {
-        contrast = 'hard',
-        transparent_mode = false,
-        dim_inactive = true,
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
       }
-      vim.cmd.colorscheme 'gruvbox'
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
